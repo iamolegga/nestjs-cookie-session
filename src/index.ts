dbd929c94c78c49742f8864bd18f64d1bb338752
Oleg Repin <iamolegga@gmail.com>
2019-10-29 18:18:47 +0300

    moved boilerplate logic to `create-nestjs-middleware-module`

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,18 @@
-export * from './CookieSessionModule';
-export {
-  NestCookieSessionOptions,
-  NestCookieSessionAsyncOptions,
-} from './options';
+import cookieSession = require('cookie-session');
+import {
+  AsyncOptions,
+  createModule,
+  SyncOptions,
+} from 'create-nestjs-middleware-module';
+
+interface Options {
+  session: Parameters<typeof cookieSession>[0];
+}
+
+export type NestCookieSessionOptions = SyncOptions<Options>;
+
+export type NestCookieSessionAsyncOptions = AsyncOptions<Options>;
+
+export const CookieSessionModule = createModule<Options>(({ session }) =>
+  cookieSession(session),
+);
